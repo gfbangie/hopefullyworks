ac8831dce49edae1d227fdc58fd697f59260d987
gfbangie <goolam.fareed123@gmail.com>
2020-03-16 20:23:25 +0200

    updated some files

---
+++ src/test/java/com/hopefullyeasy/AppTest2.java
@@ -0,0 +1,15 @@
+package com.hopefullyeasy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest2 {
+
+	@Test
+	public void testAdd() {
+		App a=new App();
+		assertEquals(a.add(1, 1),2);
+	}
+
+}
