e5a86fdd276112ad9816be10df2f68a406565021
gfbangie <61416962+gfbangie@users.noreply.github.com>
2020-03-20 15:29:26 +0200

    Update AppTest2.java

--- src/test/java/com/hopefullyeasy/AppTest2.java
+++ src/test/java/com/hopefullyeasy/AppTest2.java
@@ -5,11 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AppTest2 {
-
 	@Test
 	public void testAdd() {
-		App a=new App();
-		assertEquals(a.add(1, 1),0);
+		App calc = new App();
+		assertEquals(calc.add(1, 1), 2);
+	}
+	@Test
+	public void testSub() {
+		App calc = new App();
+		assertEquals(calc.sub(1, 1), 0);
+	}
+	@Test
+	public void testMul() {
+		App calc = new App();
+		assertEquals(calc.mul(3, 5), 15);
+	}
+	@Test
+	public void testDiv() {
+		App calc = new App();
+		assertEquals(calc.div(8, 2), 3);
 	}
 
 }
